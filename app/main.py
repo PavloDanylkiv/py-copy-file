1e89f8b0798f5c4d486fd6349465ec20103a1a60
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2026-03-28 20:23:36 +0200

    complete task

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    splited_data = command.split()
+    if len(splited_data) < 3 or splited_data[0] != "cp":
+        return
+    cmd, first_file, second_file = splited_data
+    if first_file != second_file:
+        try:
+            with (open(first_file, "r") as file_read,
+                  open(second_file, "w") as file_write):
+                file_write.write(file_read.read())
+        except FileNotFoundError:
+            return
